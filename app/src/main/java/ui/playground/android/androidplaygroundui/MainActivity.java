da2c7561c8a0c412a0daafc10036c76dc268ced3
Dimitar Danailov <dimityr.danailov@gmail.com>
2018-08-06 19:00:47 +0300

    Repo has the initial setup.

---
+++ app/src/main/java/ui/playground/android/androidplaygroundui/MainActivity.java
@@ -0,0 +1,37 @@
+package ui.playground.android.androidplaygroundui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mDatepicker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initializeDatepicker();
+    }
+
+    private void initializeDatepicker() {
+        mDatepicker = (Button) findViewById(R.id.datepicker);
+
+        mDatepicker.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                loadNewActivity(DatepickerActivity.class);
+            }
+        });
+    }
+
+    private void loadNewActivity(Class destinationActivity) {
+        Context context = MainActivity.this;
+        Intent startChildActivityIntent = new Intent(context, destinationActivity);
+        startActivity(startChildActivityIntent);
+    }
+}
