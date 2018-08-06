96a2c7c433a6e1184f96cab6b1960b52843a587e
Dimitar Danailov <dimityr.danailov@gmail.com>
2018-08-06 21:27:58 +0300

    DatepickerActivity can create a custom UIDialogFragment and style is Spinner.

--- app/src/main/java/ui/playground/android/androidplaygroundui/DatepickerActivity.java
+++ app/src/main/java/ui/playground/android/androidplaygroundui/DatepickerActivity.java
@@ -1,7 +1,11 @@
 package ui.playground.android.androidplaygroundui;
 
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import ui.playground.android.androidplaygroundui.fragments.UIDialogFragment;
 
 public class DatepickerActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class DatepickerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datepicker);
+    }
+
+    public void showDatePicker(View v) {
+        DialogFragment fragment = new UIDialogFragment();
+        fragment.show(getSupportFragmentManager(), "date picker");
     }
 }
---
+++ app/src/main/java/ui/playground/android/androidplaygroundui/fragments/UIDialogFragment.java
@@ -0,0 +1,36 @@
+package ui.playground.android.androidplaygroundui.fragments;
+
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.util.Log;
+import android.widget.DatePicker;
+
+
+import java.util.Calendar;
+import ui.playground.android.androidplaygroundui.R;
+
+public class UIDialogFragment extends DialogFragment {
+
+    private static final String TAG = UIDialogFragment.class.getName();
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                Log.d(TAG, "Selected date is: " + dayOfMonth);
+            }
+        };
+
+        return new DatePickerDialog(getActivity(), AlertDialog.THEME_HOLO_LIGHT, listener, year, month, day);
+    }
+}
